dbca1dec700dc13f7033a734584c77bc1c4d8221
Samarth Bagul <119740483+samarth-bagul@users.noreply.github.com>
2025-10-01 00:18:40 +0530

    Add files via upload

---
+++ Quick Sort.py
@@ -0,0 +1,13 @@
+# Quick Sort in Python
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr)//2]
+    left = [x for x in arr if x < pivot]
+    mid = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + mid + quick_sort(right)
+
+# Example
+arr = [10, 7, 8, 9, 1, 5]
+print("Sorted:", quick_sort(arr))
